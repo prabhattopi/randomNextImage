a957fc8e0cb80cf374636ab97a7c01e677fbeae5
prabhattopi <topi9864@gmail.com>
2023-07-08 11:59:16 -0400

    the

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,27 +36,25 @@ export default function App() {
   if (imageUrl) {
     return (
       <>
-        {typeof window !== "undefined" && (
+   
           <Head>
             <title>Random Images</title>
-            <meta name="description" content="Check out this random image!" />
+            <meta property="og:type" content="article" />
             <meta property="og:title" content="Random Images" />
-            <meta
-              property="og:description"
-              content="Check out this random image!"
-            />
             <meta property="og:image" content={imageUrl} />
-            <meta property="og:image:width" content="400" />
-            <meta property="og:image:height" content="300" />
+            <meta property="og:image:width" content="100" />
+            <meta property="og:image:height" content="100" />
+            <meta property="og:description" content="Check out this random image!" />
+            <meta property="og:url" content={`${window.location.protocol}//${window.location.host}${router.pathname}`} />
+            <meta property="og:site_name" content="Your Site Name" />
+            <meta property="article:author" content="Author Name" />
+            <meta property="article:published_time" content="2023-07-08T12:00:00Z" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content="Random Images" />
-            <meta
-              name="twitter:description"
-              content="Check out this random image!"
-            />
+            <meta name="twitter:description" content="Check out this random image!" />
             <meta name="twitter:image" content={imageUrl} />
           </Head>
-        )}
+
         <div className="flex items-center justify-center min-h-screen bg-gray-100 ">
           <div className="bg-white rounded-lg shadow-md p-4 max-w-sm w-full sm:max-w-md sm:w-auto">
             <h1 className="text-2xl font-bold mb-4">Random Image Display</h1>
