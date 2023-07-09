829cd3642de8f9568af763a1f16bf88f069c9d15
webdevelopertheworldofweb <webdevelopertheworldofweb@gmail.com>
2023-07-09 08:00:18 -0400

    the file is puehsd

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,28 +1,50 @@
-/* eslint-disable @next/next/no-img-element */
-
-import { ImageResponse } from "next/server";
-import { getData } from "./getData";
+import { ImageResponse } from 'next/server'
+import { getData } from './getData'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const alt = 'About Acme'
 export const size = {
   width: 1200,
   height: 630,
-};
-export const alt = "Expolorer | Blog";
-export const contentType = "image/png";
+}
+ 
+export const contentType = 'image/png'
+ 
+// Font
 
-export default async function og() {
- const data=await getData()
-
+ 
+// Image generation
+export default async function Image() {
+    const data=await getData()
   return new ImageResponse(
     (
-        <div className="flex flex-col">
-           
-            <img  src={data.url} alt="random image"/>
-          
-          
-             <div>Random Image</div>
-        </div>
-   
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection:"column",
+          gap:"20px"
+        }}
+      >
+          <img src={data.url} alt=""/>
+        About Acme
+      </div>
     ),
-    size
-  );
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    
+    }
+  )
 }
