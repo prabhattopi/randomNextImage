127a943be2dd7fd2326acee9133e5380bce0462d
webdevelopertheworldofweb <webdevelopertheworldofweb@gmail.com>
2023-07-11 15:15:37 -0400

    the file is  puehd

--- app/getData.ts
+++ app/getData.ts
@@ -1,5 +1,5 @@
 
-// import Cookies from 'js-cookie';
+import Cookies from 'js-cookie';
 import { cookies } from 'next/headers';
 // async function setCookies(afterId:string){
 //     Cookies.set('idimage', afterId)
@@ -21,7 +21,7 @@ export async function getData() {
 
   export async function getSingleData() {
       //@ts-ignore
-    let id:{name:string,value:string}=cookies().get("image")
+    let id:{name:string,value:string}=Cookies.get("image")
     const res = await fetch('https://picsum.photos/id/'+id.value, { cache: 'no-store' });
     const data = res
     console.log(data)
